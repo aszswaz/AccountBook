c1420834a5b56243ba4ab80d89d2ed422cb63a23
朽木不可雕也 <aszswaz@163.com>
2025-08-02 23:41:02 +0800

    开始开发消费支出表格

---
+++ Component/SpendingTable.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountBook.Component
+{
+    /// <summary>
+    /// Interaction logic for SpendingTable.xaml
+    /// </summary>
+    public partial class SpendingTable : UserControl
+    {
+        public SpendingTable()
+        {
+            InitializeComponent();
+        }
+    }
+}
